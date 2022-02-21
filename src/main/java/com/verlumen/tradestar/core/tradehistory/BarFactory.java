2aed30d3c9b3084b0aa6ccb8025390a306783b8f
Patrick Selamy <pselamy@gmail.com>
2022-02-21 17:06:34 -0600

    Improve `Bar` validation.

--- src/main/java/com/verlumen/tradestar/core/tradehistory/BarFactory.java
+++ src/main/java/com/verlumen/tradestar/core/tradehistory/BarFactory.java
@@ -1,47 +1,42 @@
 package com.verlumen.tradestar.core.tradehistory;
 
-import com.google.common.collect.ImmutableMap;
 import com.verlumen.tradestar.protos.candles.Candle;
-import com.verlumen.tradestar.protos.candles.Granularity;
 import org.ta4j.core.Bar;
 import org.ta4j.core.BaseBar;
-import org.ta4j.core.num.DecimalNum;
 
 import java.time.Duration;
 import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
-import java.util.Optional;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static org.ta4j.core.num.DecimalNum.valueOf;
 
 public class BarFactory {
-    private static final ImmutableMap<Granularity, Long> SECONDS_BY_GRANULARITY =
-            ImmutableMap.<Granularity, Long>builder()
-                    .put(Granularity.ONE_MINUTE, 60L)
-                    .put(Granularity.FIVE_MINUTES, 300L)
-                    .put(Granularity.FIFTEEN_MINUTES, 900L)
-                    .put(Granularity.ONE_HOUR, 3600L)
-                    .put(Granularity.SIX_HOURS, 21600L)
-                    .put(Granularity.ONE_DAY, 86400L)
-                    .build();
-
     static Bar create(Candle candle) {
-        Duration timePeriod = toDuration(candle.getGranularity());
+        checkArgument(candle.hasGranularity());
+        checkArgument(candle.hasStart());
+        checkArgument(candle.hasOpen());
+        checkArgument(candle.hasHigh());
+        checkArgument(candle.hasLow());
+        checkArgument(candle.hasClose());
+        checkArgument(candle.hasVolume());
+        return createBar(candle);
+    }
+
+    private static Bar createBar(Candle candle) {
+        Duration duration = GranularitySpec.get(candle.getGranularity()).duration();
         Instant start = Instant.ofEpochSecond((candle.getStart()).getSeconds());
-        ZonedDateTime end = start.plus(timePeriod).atZone(ZoneOffset.UTC);
+        ZonedDateTime end = start.plus(duration).atZone(ZoneOffset.UTC);
         return BaseBar.builder()
-                .timePeriod(timePeriod)
+                .timePeriod(duration)
                 .endTime(end)
-                .openPrice(DecimalNum.valueOf(candle.getOpen()))
-                .highPrice(DecimalNum.valueOf(candle.getHigh()))
-                .lowPrice(DecimalNum.valueOf(candle.getLow()))
-                .closePrice(DecimalNum.valueOf(candle.getClose()))
-                .volume(DecimalNum.valueOf(candle.getVolume()))
+                .openPrice(valueOf(candle.getOpen()))
+                .highPrice(valueOf(candle.getHigh()))
+                .lowPrice(valueOf(candle.getLow()))
+                .closePrice(valueOf(candle.getClose()))
+                .volume(valueOf(candle.getVolume()))
                 .build();
     }
 
-    private static Duration toDuration(Granularity granularity) {
-        return Optional.ofNullable(SECONDS_BY_GRANULARITY.get(granularity))
-                .map(Duration::ofSeconds)
-                .orElseThrow(IllegalArgumentException::new);
-    }
 }
--- src/main/java/com/verlumen/tradestar/core/tradehistory/BarSeriesFactory.java
+++ src/main/java/com/verlumen/tradestar/core/tradehistory/BarSeriesFactory.java
@@ -6,15 +6,63 @@ import org.ta4j.core.Bar;
 import org.ta4j.core.BarSeries;
 import org.ta4j.core.BaseBarSeriesBuilder;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.collect.ImmutableList.toImmutableList;
+import static java.time.Instant.ofEpochSecond;
 
 public class BarSeriesFactory {
     public static BarSeries create(ImmutableList<Candle> candles) {
+        checkArgument(candlesAreConforming(candles));
         ImmutableList<Bar> bars = candles.stream()
                 .map(BarFactory::create)
                 .collect(toImmutableList());
         return new BaseBarSeriesBuilder()
                 .withBars(bars)
                 .build();
+    }
+
+
+    public static boolean candlesAreConforming(ImmutableList<Candle> candles) {
+        return sliding(candles, 2)
+                .map(subList -> new CandlePair(subList.get(0), subList.get(1)))
+                .allMatch(CandlePair::isConforming);
+    }
+
+    public static <T> Stream<ImmutableList<T>> sliding(ImmutableList<T> list, int size) {
+        if (size > list.size())
+            return Stream.empty();
+
+        return IntStream.rangeClosed(0, list.size() - size)
+                .mapToObj(start -> list.subList(start, start + size));
+    }
+
+    private static class CandlePair {
+        private final Candle first;
+        private final Candle second;
+
+        private CandlePair(Candle first,
+                           Candle second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        private boolean isConforming() {
+            return first.getGranularity().equals(second.getGranularity()) &&
+                    candlesAreProperlySpaced(GranularitySpec
+                            .get(first.getGranularity())
+                            .duration());
+
+        }
+
+        private boolean candlesAreProperlySpaced(Duration expectedDuration) {
+            Instant instant1 = ofEpochSecond(first.getStart().getSeconds());
+            Instant instant2 = ofEpochSecond(second.getStart().getSeconds());
+            return Duration.between(instant1, instant2).equals(expectedDuration);
+        }
     }
 }
