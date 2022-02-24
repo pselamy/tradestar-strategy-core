e91d109cee42c28091daaf91d0f07bbb650f9fd1
Patrick Selamy <pselamy@gmail.com>
2022-02-23 20:35:00 -0600

    Add `IndicatorFactory`.

---
+++ src/main/java/com/verlumen/tradestar/core/strategies/IndicatorFactory.java
@@ -0,0 +1,30 @@
+package com.verlumen.tradestar.core.strategies;
+
+import com.verlumen.tradestar.protos.strategies.TradeStrategy;
+import org.ta4j.core.BarSeries;
+import org.ta4j.core.Indicator;
+import org.ta4j.core.num.Num;
+
+import java.util.function.BiFunction;
+
+interface IndicatorFactory {
+    static IndicatorFactory create(
+            TradeStrategy.StrategyOneOfCase supportedCase,
+            BiFunction<BarSeries, Integer, Indicator<Num>> indicatorFunction) {
+        return new IndicatorFactory() {
+            @Override
+            public Indicator<Num> create(BarSeries barSeries, int barCount) {
+                return indicatorFunction.apply(barSeries, barCount);
+            }
+
+            @Override
+            public TradeStrategy.StrategyOneOfCase supportedCase() {
+                return supportedCase;
+            }
+        };
+    }
+
+    Indicator<Num> create(BarSeries barSeries, int barCount);
+
+    TradeStrategy.StrategyOneOfCase supportedCase();
+}
