8569204c00e0d0aef75fbf339a3ed01779369d0f
Patrick Selamy <pselamy@gmail.com>
2022-02-23 21:34:41 -0600

    Add `IndicatorFactoryProvider`.

---
+++ src/main/java/com/verlumen/tradestar/core/strategies/IndicatorFactoryProvider.java
@@ -0,0 +1,27 @@
+package com.verlumen.tradestar.core.strategies;
+
+import com.google.inject.Provider;
+import com.verlumen.tradestar.protos.strategies.TradeStrategy;
+import org.ta4j.core.BarSeries;
+import org.ta4j.core.Indicator;
+import org.ta4j.core.indicators.adx.ADXIndicator;
+import org.ta4j.core.num.Num;
+
+import java.util.function.BiFunction;
+
+enum IndicatorFactoryProvider implements Provider<IndicatorFactory> {
+    ADX(ADXIndicator::new);
+
+    private final BiFunction<BarSeries, Integer, Indicator<Num>> indicatorFunction;
+
+    IndicatorFactoryProvider(BiFunction<BarSeries, Integer, Indicator<Num>> indicatorFunction) {
+        this.indicatorFunction = indicatorFunction;
+    }
+
+    @Override
+    public IndicatorFactory get() {
+        TradeStrategy.StrategyOneOfCase supportedCase =
+                TradeStrategy.StrategyOneOfCase.valueOf(name());
+        return IndicatorFactory.create(supportedCase, indicatorFunction);
+    }
+}
