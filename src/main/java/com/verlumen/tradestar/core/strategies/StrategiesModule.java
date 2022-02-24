213d3e5c011980f18b473832aaf87920ad3d9744
Patrick Selamy <pselamy@gmail.com>
2022-02-24 17:36:04 -0600

    Update `StrategiesModule`.

--- src/main/java/com/verlumen/tradestar/core/strategies/StrategiesModule.java
+++ src/main/java/com/verlumen/tradestar/core/strategies/StrategiesModule.java
@@ -1,13 +1,21 @@
 package com.verlumen.tradestar.core.strategies;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.multibindings.Multibinder;
 
 import static com.google.inject.multibindings.Multibinder.newSetBinder;
+import static java.util.EnumSet.allOf;
 
 public class StrategiesModule extends AbstractModule {
     @Override
     protected void configure() {
-        newSetBinder(binder(), StrategyConverter.class)
-                .addBinding().to(AdxStrategyConverter.class);
+        Multibinder<IndicatorFactory> indicatorFactoryBinder =
+                newSetBinder(binder(), IndicatorFactory.class);
+        Multibinder<RuleFactory> ruleFactoryBinder =
+                newSetBinder(binder(), RuleFactory.class);
+        allOf(IndicatorFactoryProvider.class)
+                .forEach(indicatorFactoryBinder.addBinding()::toProvider);
+        ruleFactoryBinder.addBinding().to(AdxRuleFactory.class);
     }
+
 }
