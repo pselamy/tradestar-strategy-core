f263881db671585aa315a90e3e6fbf81d3b22f4c
Patrick Selamy <pselamy@gmail.com>
2022-02-24 17:33:51 -0600

    Update `StrategyFactory`.

--- src/test/java/com/verlumen/tradestar/core/strategies/StrategyFactoryTest.java
+++ src/test/java/com/verlumen/tradestar/core/strategies/StrategyFactoryTest.java
@@ -1,49 +1,36 @@
 package com.verlumen.tradestar.core.strategies;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
-import com.verlumen.tradestar.protos.strategies.TradeStrategy.ADX;
-import com.verlumen.tradestar.protos.strategies.TradeStrategy.Composite;
+import com.google.inject.multibindings.Multibinder;
+import com.verlumen.tradestar.protos.strategies.SignalStrength;
 import com.verlumen.tradestar.protos.strategies.TradeStrategy;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.ta4j.core.*;
-import org.ta4j.core.rules.BooleanRule;
 
-import java.util.function.Function;
+import java.util.EnumSet;
 
-import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.TruthJUnit.assume;
 import static com.google.inject.multibindings.Multibinder.newSetBinder;
-import static java.util.Arrays.stream;
-import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class StrategyFactoryTest {
-    private static final ImmutableMap<TradeStrategy.StrategyOneOfCase,
-            TradeStrategy> TRADE_STRATEGIES =
-            ImmutableMap.<TradeStrategy.StrategyOneOfCase,
-                            TradeStrategy>builder()
-                    .put(TradeStrategy.StrategyOneOfCase.COMPOSITE,
-                            TradeStrategy.newBuilder()
-                                    .setComposite(Composite.getDefaultInstance())
-                                    .build())
-                    .put(TradeStrategy.StrategyOneOfCase.ADX,
-                            TradeStrategy.newBuilder()
-                                    .setAdx(ADX.getDefaultInstance())
-                                    .build())
-                    .buildOrThrow();
-    private static final ImmutableMap<TradeStrategy.StrategyOneOfCase, FakeStrategyConverter> STRATEGY_CONVERTERS =
-            stream(TradeStrategy.StrategyOneOfCase.values())
-                    .filter(oneOfCase -> !oneOfCase.equals(TradeStrategy.StrategyOneOfCase.STRATEGYONEOF_NOT_SET))
-                    .collect(toImmutableMap(Function.identity(), FakeStrategyConverter::new));
+    private static final Rule BUY_RULE = (i, tradingRecord) -> false;
+    private static final Rule SELL_RULE = (i, tradingRecord) -> true;
 
-    private static final BaseBarSeries FAKE_BAR_SERIES = new BaseBarSeriesBuilder().build();
+    private static final BaseBarSeries FAKE_BAR_SERIES =
+            new BaseBarSeriesBuilder().build();
+    private static final TradeStrategy ADX_TRADE_STRATEGY =
+            TradeStrategy.newBuilder()
+                    .setAdx(TradeStrategy.ADX.newBuilder()
+                            .setBarCount(123)
+                            .setBuySignalStrength(SignalStrength.STRONG)
+                            .setSellSignalStrength(SignalStrength.WEAK))
+                    .build();
 
     @Inject
     private StrategyFactory strategyFactory;
@@ -53,43 +40,51 @@ class StrategyFactoryTest {
         Guice.createInjector(new TestModule()).injectMembers(this);
     }
 
-    @Test
-    void create_withNoTradeStrategy_throwsUnsupportedOperationException() {
-        assertThrows(UnsupportedOperationException.class,
-                () -> strategyFactory.create(FAKE_BAR_SERIES,
-                        TradeStrategy.getDefaultInstance()));
+    @ParameterizedTest
+    @EnumSource(UnsupportedTradeStrategyTestCase.class)
+    void create_withUnsupportedTradeStrategy_throwsIllegalArgumentException(
+            UnsupportedTradeStrategyTestCase testCase) {
+        assertThrows(IllegalArgumentException.class,
+                () -> strategyFactory.create(testCase.tradeStrategy,
+                        FAKE_BAR_SERIES));
     }
 
     @ParameterizedTest
     @EnumSource(TradeStrategy.StrategyOneOfCase.class)
-    void create_withTradeStrategy_CreatesStrategyWithCorrectConverter(TradeStrategy.StrategyOneOfCase oneOfCase) {
+    void create_withTradeStrategy_createsTa4jStrategy(
+            TradeStrategy.StrategyOneOfCase oneOfCase) {
         assume().that(oneOfCase).isNotEqualTo(
                 TradeStrategy.StrategyOneOfCase.STRATEGYONEOF_NOT_SET);
-        FakeStrategyConverter fakeStrategyConverter =
-                requireNonNull(STRATEGY_CONVERTERS.get(oneOfCase));
-        TradeStrategy fakeTradeStrategy =
-                requireNonNull(TRADE_STRATEGIES.get(oneOfCase));
-        Strategy expected = fakeStrategyConverter.fakeStrategy;
+        assume().that(oneOfCase).isNotEqualTo(
+                TradeStrategy.StrategyOneOfCase.COMPOSITE);
+        TradeStrategy fakeTradeStrategy = TestTradeStrategy.get(oneOfCase);
+        String expectedName = "ADX-123-BUY-STRONG-SELL-WEAK";
 
-        Strategy strategy = strategyFactory.create(FAKE_BAR_SERIES,
-                fakeTradeStrategy);
+        Strategy strategy = strategyFactory.create(fakeTradeStrategy,
+                FAKE_BAR_SERIES);
 
-        assertThat(strategy).isEqualTo(expected);
+        assertThat(strategy.getName()).isEqualTo(expectedName);
+        assertThat(strategy.getEntryRule()).isSameInstanceAs(BUY_RULE);
+        assertThat(strategy.getExitRule()).isSameInstanceAs(SELL_RULE);
     }
 
-    private static class FakeStrategyConverter implements StrategyConverter {
-        private final Strategy fakeStrategy;
+    private enum FakeRuleFactory implements RuleFactory {
+        ADX(TradeStrategy.StrategyOneOfCase.ADX);
+
         private final TradeStrategy.StrategyOneOfCase supportedCase;
 
-        FakeStrategyConverter(TradeStrategy.StrategyOneOfCase supportedCase) {
-            this.fakeStrategy = new BaseStrategy(supportedCase.name(),
-                    new BooleanRule(false), new BooleanRule(true));
+        FakeRuleFactory(TradeStrategy.StrategyOneOfCase supportedCase) {
             this.supportedCase = supportedCase;
         }
 
         @Override
-        public Strategy convert(TradeStrategy tradeStrategy, BarSeries barSeries) {
-            return fakeStrategy;
+        public Rule buyRule(TradeStrategy params, BarSeries barSeries) {
+            return BUY_RULE;
+        }
+
+        @Override
+        public Rule sellRule(TradeStrategy params, BarSeries barSeries) {
+            return SELL_RULE;
         }
 
         @Override
@@ -98,15 +93,45 @@ class StrategyFactoryTest {
         }
     }
 
+    private enum TestTradeStrategy {
+        ADX(ADX_TRADE_STRATEGY);
+
+        private final TradeStrategy tradeStrategy;
+
+        TestTradeStrategy(TradeStrategy tradeStrategy) {
+            this.tradeStrategy = tradeStrategy;
+        }
+
+        public static TradeStrategy get(
+                TradeStrategy.StrategyOneOfCase oneOfCase) {
+            return valueOf(oneOfCase.name()).tradeStrategy;
+        }
+    }
+
+    private enum UnsupportedTradeStrategyTestCase {
+        STRATEGYONEOF_NOT_SET(TradeStrategy.getDefaultInstance()),
+        COMPOSITE(TradeStrategy.newBuilder()
+                .setComposite(TradeStrategy.Composite.newBuilder()
+                        .addStrategies(ADX_TRADE_STRATEGY))
+                .build());
+
+        private final TradeStrategy tradeStrategy;
+        private final TradeStrategy.StrategyOneOfCase oneOfCase;
+
+        UnsupportedTradeStrategyTestCase(TradeStrategy tradeStrategy) {
+            this.tradeStrategy = tradeStrategy;
+            this.oneOfCase = TradeStrategy.StrategyOneOfCase.valueOf(name());
+        }
+    }
+
     private static class TestModule extends AbstractModule {
         @Override
         protected void configure() {
-            STRATEGY_CONVERTERS.values().forEach(this::bindConverter);
+            Multibinder<RuleFactory> ruleFactoryBinder =
+                    newSetBinder(binder(), RuleFactory.class);
+            EnumSet.allOf(FakeRuleFactory.class).forEach(
+                    ruleFactoryBinder.addBinding()::toInstance);
         }
 
-        private void bindConverter(StrategyConverter converter) {
-            newSetBinder(binder(), StrategyConverter.class)
-                    .addBinding().toInstance(converter);
-        }
     }
 }
