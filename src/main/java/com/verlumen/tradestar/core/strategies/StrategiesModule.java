89049e4efdf38d2498a10d8970f9acd2a2ef6a67
Patrick Selamy <pselamy@gmail.com>
2022-03-01 19:08:44 -0600

    Use SignalStrengthModule.

--- src/main/java/com/verlumen/tradestar/core/strategies/StrategiesModule.java
+++ src/main/java/com/verlumen/tradestar/core/strategies/StrategiesModule.java
@@ -2,6 +2,7 @@ package com.verlumen.tradestar.core.strategies;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.multibindings.Multibinder;
+import com.verlumen.tradestar.core.signalstrength.SignalStrengthModule;
 
 import static com.google.inject.multibindings.Multibinder.newSetBinder;
 import static java.util.EnumSet.allOf;
@@ -9,6 +10,8 @@ import static java.util.EnumSet.allOf;
 public class StrategiesModule extends AbstractModule {
     @Override
     protected void configure() {
+        install(new SignalStrengthModule());
+
         bind(StrategyFactory.class).to(StrategyFactoryImpl.class);
 
         Multibinder<IndicatorFactory> indicatorFactoryBinder =
