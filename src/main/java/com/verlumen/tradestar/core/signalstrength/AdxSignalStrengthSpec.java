62c89e5bb6915a0f23e0010918cb56bb32ee1e08
Patrick Selamy <pselamy@gmail.com>
2022-03-01 18:48:38 -0600

    Add `AdxSignalStrengthSpec`.

---
+++ src/main/java/com/verlumen/tradestar/core/signalstrength/AdxSignalStrengthSpec.java
@@ -0,0 +1,41 @@
+package com.verlumen.tradestar.core.signalstrength;
+
+import com.google.common.collect.Range;
+import com.verlumen.tradestar.protos.strategies.SignalStrength;
+import com.verlumen.tradestar.protos.strategies.TradeStrategy;
+import org.ta4j.core.num.DecimalNum;
+import org.ta4j.core.num.Num;
+
+public class AdxSignalStrengthSpec implements SignalStrengthSpec {
+    @Override
+    public Range<Num> range(SignalStrength signalStrength) {
+        return Spec.get(signalStrength).range;
+    }
+
+    @Override
+    public TradeStrategy.StrategyOneOfCase supportedCase() {
+        return TradeStrategy.StrategyOneOfCase.ADX;
+    }
+
+    @SuppressWarnings("unused")
+    private enum Spec {
+        WEAK(0, 25),
+        STRONG(25, 50),
+        VERY_STRONG(50, 75),
+        EXTREMELY_STRONG(75, 100);
+
+        private final Range<Num> range;
+
+        Spec(int lower, int upper) {
+            this.range = Range.closedOpen(decimalNum(lower), decimalNum(upper));
+        }
+
+        private static Spec get(SignalStrength signalStrength) {
+            return Spec.valueOf(signalStrength.name());
+        }
+
+        private static Num decimalNum(Number number) {
+            return DecimalNum.valueOf(number);
+        }
+    }
+}
