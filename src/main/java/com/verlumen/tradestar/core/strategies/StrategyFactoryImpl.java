39086a7ca9c7bfd4b459dadb983f3ee41df2e7d4
Patrick Selamy <pselamy@gmail.com>
2022-03-02 21:38:03 -0600

    Update StrategyFactoryImpl.

--- src/main/java/com/verlumen/tradestar/core/strategies/StrategyFactoryImpl.java
+++ src/main/java/com/verlumen/tradestar/core/strategies/StrategyFactoryImpl.java
@@ -1,7 +1,6 @@
 package com.verlumen.tradestar.core.strategies;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
 import com.google.inject.Inject;
 import com.verlumen.tradestar.core.rules.RuleFactory;
 import com.verlumen.tradestar.protos.strategies.TradeStrategy;
@@ -11,19 +10,12 @@ import org.ta4j.core.BaseStrategy;
 import org.ta4j.core.Rule;
 import org.ta4j.core.Strategy;
 
-import java.util.EnumSet;
 import java.util.Optional;
 import java.util.Set;
 
-import static com.google.common.base.Preconditions.checkArgument;
 import static com.verlumen.tradestar.core.strategies.StrategyOneOfCases.keyBySupportedCase;
 
 class StrategyFactoryImpl implements StrategyFactory {
-    private static final ImmutableSet<StrategyOneOfCase>
-            SUPPORTED_CASES = ImmutableSet.copyOf(EnumSet.complementOf(
-            EnumSet.of(StrategyOneOfCase.STRATEGYONEOF_NOT_SET,
-                    StrategyOneOfCase.COMPOSITE)));
-
     private final ImmutableMap<StrategyOneOfCase, RuleFactory> ruleFactories;
 
     @Inject
@@ -38,7 +30,6 @@ class StrategyFactoryImpl implements StrategyFactory {
 
     @Override
     public Strategy create(TradeStrategy params, BarSeries barSeries) {
-        checkArgument(SUPPORTED_CASES.contains(params.getStrategyOneOfCase()));
         String name = getStrategyName(params);
         RuleFactory ruleFactory = ruleFactory(params.getStrategyOneOfCase());
         Rule entryRule = ruleFactory.buyRule(params, barSeries);
